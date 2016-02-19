d71bf9d3134ddc364171f802f8cdde9ff58dbb47
IAlexandr <aivanov@informatica.ru>
2016-02-19 17:31:46 +0300

    parent-pinger

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-// var http = require('http');
-// var api = require('./lib/api');
-// var config = require('./config');
-// var l = require('./lib/logger');
-//
-// var wsServer = require('./lib/wserver');
-// var wsClient = require('./lib/wclient');
-//
-// var httpServer = http.Server(api());
-// //wsServer.listen(httpServer);
-// wsClient.connect();
-// httpServer.listen(config.about.apiPort);
-// l.log('HTTP API listening on port: ' + config.about.apiPort);
-
 require('babel-register');
 require('./lib/server');
