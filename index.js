35158399d96b01e8ad09f5705e6c596434f02524
IAlexandr <aivanov@informatica.ru>
2016-02-19 15:37:05 +0300

    ws server client

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var wsServer = require('./lib/wserver');
 var wsClient = require('./lib/wclient');
 
 var httpServer = http.Server(api());
-wsServer.listen(httpServer);
+//wsServer.listen(httpServer);
 wsClient.connect();
 httpServer.listen(config.about.apiPort);
 l.log('HTTP API listening on port: ' + config.about.apiPort);
